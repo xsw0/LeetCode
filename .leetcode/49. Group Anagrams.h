c19daac62cd6c2275ce85103a8879578e9e3a020
xsw0 <xsw.mail.0@gmail.com>
2021-05-21 04:21:00 +0800

    51

--- .leetcode/49. Group Anagrams.h
+++ .leetcode/49. Group Anagrams.h
@@ -1,14 +1,34 @@
 class Solution
 {
+    string hash(const string &s)
+    {
+        string result;
+        result.reserve(s.size());
+        array<uint8_t, 26> arr{};
+        for (auto c : s)
+        {
+            ++arr[c - 'a'];
+        }
+        for (size_t i = 0; i < arr.size(); ++i)
+        {
+            result.resize(result.size() + arr[i], 'a' + i);
+        }
+        return result;
+    }
  public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-        unordered_map<unordered_set<char>, vector<string>> um;
+        unordered_map<string, vector<string>> um;
         for (const auto &s :strs)
         {
-            unordered_set<char> us{ s.cbegin(), s.cend() };
-            um[us].push_back(s);
+            um[hash(s)].push_back(s);
         }
-        return vector<vector<string>>(um.cbegin(), um.cend());
+        vector<vector<string>> result;
+        result.reserve(um.size());
+        for (const auto &p : um)
+        {
+            result.emplace_back(p.second);
+        }
+        return result;
     }
 };
--- main.cpp
+++ main.cpp
@@ -2,17 +2,57 @@
 
 #include "bits/stdc++.h"
 #include "ListNode.h"
-#include "to_string.h"
+//#include "to_string.h"
 
 using namespace std;
 
-#include ".leetcode/49. Group Anagrams.h"
+template<typename It>
+std::string ss(It first,
+		It last,
+		function<std::string(const typename std::iterator_traits<It>::value_type&)> p,
+		const std::string& separator = ",")
+{
+	std::string result;
+	if (first != last)
+	{
+		result += p(*first);
+		++first;
+		while (first != last)
+		{
+			result += separator + p(*first);
+			++first;
+		}
+	}
+	return result;
+}
+
+#include ".leetcode/51. N-Queens.h"
 
 int main()
 {
-    Solution solution;
-    auto arg1 = vector<vector<int>>{{ 5, 1, 9, 11 }, { 2, 4, 8, 10 }, { 13, 3, 6, 7 }, { 15, 14, 12, 16 }};
-    solution.rotate(arg1);
-    cout << arg1 << endl;
-    return 0;
+//    TO_STRING::CONSOLE_WIDTH = 10;
+	Solution solution;
+
+//    auto arg1 = vector<string>{ "eat", "tea", "tan", "ate", "nat", "bat" };
+//    cout << arg1 << endl;
+//    cout <<
+//         to_string(solution.solveNQueens(1))
+//         << endl;
+
+
+	for (size_t i = 1; i <= 9; ++i)
+	{
+		nQueens nq{ i };
+		nq.solve();
+		cout << nq.to_string() << "," << endl << endl;
+//        for (auto &board : nq.queens)
+//        {
+//            for (size_t row = 0; row < board.size(); ++row)
+//            {
+////                cout << " ( " << row << " " << board[row] << " ) ";
+//            }
+//            cout << endl;
+//        }
+	}
+	return 0;
 }
