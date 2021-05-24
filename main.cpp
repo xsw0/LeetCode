b3c4a2764f483b4bfbe4f21d9ead53ea2f24536f
xsw0 <xsw.mail.0@gmail.com>
2021-05-24 18:46:11 +0800

    76

--- main.cpp
+++ main.cpp
@@ -5,48 +5,23 @@
 
 using namespace std;
 
-#include ".leetcode/51. N-Queens.h"
+#include ".leetcode/76. Minimum Window Substring.h"
 
 int main()
 {
-//    Solution solution;
-//    auto arg1 = vector<int>{ 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-//    auto arg2 = "babadada";
-//    cout <<
-//         //         to_string(
-//         solution.trap(
-//             arg1
-//         )
-//         //         )
-//         << endl;
-
-    TO_STRING::CONSOLE_WIDTH = 100000;
-    TO_STRING::arrayPrefix = "{{";
-    TO_STRING::arraySuffix = "}}";
-//    vector<vector<vector<string>>> v;
-//    for (int i = 1; i <= 7; ++i)
-//    {
-//        v.push_back(Solution::solve(Solution::nQueens(i).queens));
-//    }
-//    cout << to_string(v);
-//    for (int i = 8; i <= 9; ++i)
-//    {
-//        cout << to_string(Solution::nQueens(i).queens);
-//    }
-//    for (auto i = 1; i <= 7; ++i)
-//    {
-//        cout << "case " << i << ": return vector<vector<string>>"
-//             << to_string(Solution::solve(Solution::nQueens(i).queens))
-//             << ";\n";
-//    }
-
-    for (auto i = 8; i <= 9; ++i)
-    {
-        auto x = Solution::nQueens(i).queens;
-        cout << "case " << i << ": return solve<" << i << "," << x.size()
-             << ">({"
-             << to_string(Solution::nQueens(i).queens)
-             << "});\n";
-    }
+    Solution solution;
+    auto arg1 = vector<int>{ 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+    auto arg2 = "babadada";
+    auto arg3 = vector<vector<int>>{
+        { 1, 1 }
+    };
+    cout <<
+         //         to_string(
+         solution.minWindow(
+             "ADOBECODEBANC",
+             "ABC"
+         )
+         //         )
+         << endl;
     return 0;
 }
