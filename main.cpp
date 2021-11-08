a108faeab26ea2189ad1aeb10490859aa3d2c4d8
xsw0 <xsw.mail.0@gmail.com>
2021-11-08 23:41:24 +0800

    81

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include "bits/stdc++.h"
 
+#include "ListNode.h"
+
 using namespace std;
 
-#include ".leetcode/80. Remove Duplicates from Sorted Array II.h"
+#include ".leetcode/81. Search in Rotated Sorted Array II.h"
 
 int main()
 {
     Solution solution;
 
-    auto arg1 = vector<int>{ 1, 1, 1, 2, 2, 3 };
-    auto arg2 = "";
+    auto arg1 = vector<int>{ 1, 2, 3, 3, 4, 4, 5 };
+    auto arg2 = 2;
+
+    ListNode* ld = ListNode::pListNode(arg1.begin(), arg1.end());
 
     cout <<
-         solution.removeDuplicates(arg1)
+         solution.deleteDuplicates(
+             ld
+         )
          << endl;
     return 0;
 }
